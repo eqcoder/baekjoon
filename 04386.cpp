1d95a50eb8350d46ce62caece0f3477774b419b2
eqcoder <tmdvy830@unist.ac.kr>
2023-01-28 14:20:24 +0900

    union find

--- 04386.cpp
+++ 04386.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <vector>
 
+using namespace std;
+
+int n;
+
 int main()
 {
+    cin>>n;
+    for(int i=0;i<n;i++){
+        
+    }
 }
--- 10775_.cpp
+++ 10775_.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
 
-int main dd
-sdaadd
+using namespace std;
+
+int gate[100001], g, p;
+
+int find(int x)
+{
+    if (gate[x] == x)
+        return x;
+    return gate[x] = find(gate[x]);
+}
+
+void Union(int x, int y)
+{
+    x = find(x);
+    y = find(y);
+    gate[x] = y;
+    return;
+}
+
+int main()
+{
+    int k, ans = 0;
+    cin >> g >> p;
+    for (int i = 0; i <= g; i++)
+        gate[i] = i;
+    for (int i = 0; i < p; i++)
+    {
+        cin >> g;
+        k = find(g);
+        if (!k)
+            break;
+        Union(k, k - 1);
+        ans++;
+    }
+    cout << ans;
+    return 0;
+}
